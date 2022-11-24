a3e9a9bb91bbe98b7aa481be03df3e2c75dd048f
Ismail Marmoush <marmoushismail@gmail.com>
2022-11-24 08:40:14 +0100

    adding domain constructor

--- active/src/main/java/io/memoria/atom/active/eventsourcing/pipeline/Domain.java
+++ active/src/main/java/io/memoria/atom/active/eventsourcing/pipeline/Domain.java
@@ -6,6 +6,7 @@ import io.memoria.atom.core.eventsourcing.State;
 import io.memoria.atom.core.eventsourcing.rule.Decider;
 import io.memoria.atom.core.eventsourcing.rule.Evolver;
 import io.memoria.atom.core.eventsourcing.rule.Saga;
+import io.vavr.control.Option;
 
 public record Domain<S extends State, C extends Command, E extends Event>(Class<S> sClass,
                                                                           Class<C> cClass,
@@ -13,4 +14,13 @@ public record Domain<S extends State, C extends Command, E extends Event>(Class<
                                                                           S initState,
                                                                           Decider<S, C, E> decider,
                                                                           Saga<E, C> saga,
-                                                                          Evolver<S, E> evolver) {}
+                                                                          Evolver<S, E> evolver) {
+  public Domain(Class<S> sClass,
+                Class<C> cClass,
+                Class<E> eClass,
+                S initState,
+                Decider<S, C, E> decider,
+                Evolver<S, E> evolver) {
+    this(sClass, cClass, eClass, initState, decider, e -> Option.none(), evolver);
+  }
+}
