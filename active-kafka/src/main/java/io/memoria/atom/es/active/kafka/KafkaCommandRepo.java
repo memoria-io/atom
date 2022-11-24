d9c1169e25a06df99c16a411748809ad6826e97a
Ismail Marmoush <marmoushismail@gmail.com>
2022-11-24 02:37:27 +0100

    get rid of msg

--- active-kafka/src/main/java/io/memoria/atom/es/active/kafka/KafkaCommandRepo.java
+++ active-kafka/src/main/java/io/memoria/atom/es/active/kafka/KafkaCommandRepo.java
@@ -2,51 +2,64 @@ package io.memoria.atom.es.active.kafka;
 
 import io.memoria.atom.core.eventsourcing.Command;
 import io.memoria.atom.core.text.TextTransformer;
-import io.memoria.atom.es.active.kafka.client.KafkaClient;
-import io.memoria.atom.es.active.kafka.client.Msg;
+import io.memoria.atom.es.active.kafka.infra.KafkaUtils;
 import io.memoria.atom.es.active.repo.CommandRepo;
+import io.vavr.collection.Map;
 import io.vavr.control.Try;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.time.Duration;
 import java.util.stream.Stream;
 
 public class KafkaCommandRepo<C extends Command> implements CommandRepo<C> {
   private final String topic;
   private final int streamPartition;
   private final int totalPartitions;
+
   private final Class<C> cClass;
   private final TextTransformer transformer;
-  private final KafkaClient client;
+  private final Duration pollDuration;
+  private final KafkaProducer<String, String> producer;
+  private final KafkaConsumer<String, String> consumer;
 
   public KafkaCommandRepo(String topic,
                           int partition,
                           int totalPartitions,
                           Class<C> cClass,
                           TextTransformer transformer,
-                          KafkaClient client) {
+                          Duration pollDuration,
+                          Map<String, Object> producerConfig,
+                          Map<String, Object> consumerConfig) {
     this.topic = topic;
     this.streamPartition = partition;
     this.totalPartitions = totalPartitions;
     this.cClass = cClass;
     this.transformer = transformer;
-    this.client = client;
+    this.pollDuration = pollDuration;
+    this.producer = new KafkaProducer<>(producerConfig.toJavaMap());
+    this.consumer = new KafkaConsumer<>(consumerConfig.toJavaMap());
   }
 
   @Override
   public Stream<Try<C>> stream() {
-    return client.stream(topic, streamPartition).map(this::toCommand);
+    return KafkaUtils.stream(consumer, topic, streamPartition, pollDuration).map(this::toCommand);
   }
 
   @Override
   public Try<C> push(C cmd) {
-    var partition = cmd.partition(totalPartitions);
-    var key = cmd.commandId().value();
-    return transformer.serialize(cmd)
-                      .map(body -> new Msg(topic, partition, key, body))
-                      .flatMap(client::push)
-                      .map(msg -> cmd);
+    return toRecord(cmd).flatMap(c -> Try.of(() -> KafkaUtils.send(producer, c))).map(meta -> cmd);
   }
 
-  private Try<C> toCommand(Msg msg) {
-    return transformer.deserialize(msg.body(), cClass);
+  public Try<ProducerRecord<String, String>> toRecord(C cmd) {
+    var partition = cmd.partition(totalPartitions);
+    var key = cmd.commandId().value();
+    return transformer.serialize(cmd).map(b -> new ProducerRecord<>(topic, partition, key, b));
+  }
+
+  private Try<C> toCommand(ConsumerRecord<String, String> msg) {
+    return transformer.deserialize(msg.value(), cClass);
   }
 }
