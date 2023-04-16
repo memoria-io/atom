50bbcef9c30c90eb213690d2122571f8e800f2c2
Ismail Marmoush <marmoushismail@gmail.com>
2023-04-17 01:55:40 +0200

    adding Id tests

--- core/src/test/java/io/memoria/atom/core/id/IdTest.java
+++ core/src/test/java/io/memoria/atom/core/id/IdTest.java
@@ -1,5 +1,6 @@
 package io.memoria.atom.core.id;
 
+import com.github.f4b6a3.uuid.UuidCreator;
 import io.vavr.collection.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,15 +10,27 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class IdTest {
+  @Test
+  void happyPath(){
+    var id = Id.of(UuidCreator.getTimeOrderedEpoch());
+    Assertions.assertThat(id.value()).isNotEmpty();
+  }
+
+  @Test
+  void implementationTypes(){
+    Assertions.assertThat(Id.of("0")).isInstanceOf(SeqId.class);
+    Assertions.assertThat(Id.of(UuidCreator.getTimeOrderedEpoch())).isInstanceOf(TimedUUID.class);
+    Assertions.assertThat(Id.of("bob")).isInstanceOf(StringId.class);
+  }
 
   @Test
   void validation() {
     UUID nullUUID = null;
     var randomUUID = UUID.randomUUID();
-    Assertions.assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> Id.of(nullUUID));
-    Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Id.of(randomUUID));
-    Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Id.of(-1L));
-    Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Id.of(""));
+    Assertions.assertThatNullPointerException().isThrownBy(() -> Id.of(nullUUID));
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Id.of(randomUUID));
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Id.of(-1L));
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Id.of(""));
   }
 
   @Test
