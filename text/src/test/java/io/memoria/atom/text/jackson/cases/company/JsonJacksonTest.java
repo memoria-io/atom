99b1e2126149d784504b2a2191ab8e397438a457
Ismail Marmoush <marmoushismail@gmail.com>
2023-04-17 17:03:39 +0200

    higher order fn serdes

--- text/src/test/java/io/memoria/atom/text/jackson/cases/company/JsonJacksonTest.java
+++ text/src/test/java/io/memoria/atom/text/jackson/cases/company/JsonJacksonTest.java
@@ -3,6 +3,7 @@ package io.memoria.atom.text.jackson.cases.company;
 import io.memoria.atom.text.jackson.Resources;
 import io.memoria.atom.text.jackson.TestDeps;
 import io.vavr.collection.List;
+import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +21,16 @@ class JsonJacksonTest {
     var yamlEngineer = TestDeps.json.serialize(Resources.ANNIKA_MANAGER).get();
     assert Resources.ANNIKA_MANAGER_JSON != null;
     assertEquals(Resources.ANNIKA_MANAGER_JSON, yamlEngineer);
+  }
+
+  @Test
+  void serializeByHOF() {
+    // Given
+    var serializer = TestDeps.json.serialize();
+    // When
+    var managers = List.range(0, 100).map(i -> Resources.ANNIKA_MANAGER).map(serializer).map(Try::get);
+    // Then
+    managers.forEach(manager -> assertEquals(Resources.ANNIKA_MANAGER_JSON, manager));
   }
 
   @Test
@@ -50,5 +61,18 @@ class JsonJacksonTest {
     // Then
     assertEquals(Resources.ANNIKA_MANAGER, manager);
     assertEquals(List.of(Resources.BOB_ENGINEER, Resources.ALEX_ENGINEER), manager.team());
+  }
+
+  @Test
+  void deserializeByHOF() {
+    // Given
+    var toManager = TestDeps.json.deserialize(Manager.class);
+    // When
+    var managers = List.range(0, 100).map(i -> Resources.ANNIKA_MANAGER_JSON).map(toManager).map(Try::get);
+    // Then
+    managers.forEach(manager -> {
+      assertEquals(Resources.ANNIKA_MANAGER, manager);
+      assertEquals(List.of(Resources.BOB_ENGINEER, Resources.ALEX_ENGINEER), manager.team());
+    });
   }
 }
