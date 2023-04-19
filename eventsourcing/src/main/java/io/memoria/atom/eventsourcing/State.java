f932ebd0bd13d3c3549e68d1523e2b7d863bb900
Ismail Marmoush <marmoushismail@gmail.com>
2023-04-19 06:01:19 +0200

    state extends shardable

--- eventsourcing/src/main/java/io/memoria/atom/eventsourcing/State.java
+++ eventsourcing/src/main/java/io/memoria/atom/eventsourcing/State.java
@@ -1,9 +1,5 @@
 package io.memoria.atom.eventsourcing;
 
-import io.memoria.atom.core.id.Id;
-
 import java.io.Serializable;
 
-public interface State extends Shardable, Serializable {
-  Id stateId();
-}
+public interface State extends Shardable, Serializable {}
