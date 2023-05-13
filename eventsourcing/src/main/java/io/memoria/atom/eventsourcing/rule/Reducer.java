7c0b079b3007d7a0ac4447c2991bd6956684ea09
Ismail Marmoush <marmoushismail@gmail.com>
2023-05-13 17:22:08 +0200

    Removing reducer

--- eventsourcing/src/main/java/io/memoria/atom/eventsourcing/rule/Reducer.java
+++
@@ -1,8 +0,0 @@
-package io.memoria.atom.eventsourcing.rule;
-
-import io.memoria.atom.eventsourcing.Event;
-import io.memoria.atom.eventsourcing.State;
-import io.vavr.Function1;
-
-@FunctionalInterface
-public interface Reducer<S extends State, E extends Event> extends Function1<S, E> {}
