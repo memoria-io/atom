5b7d1059c8d191fcb47941878ffef476f6809f4b
Ismail Marmoush <marmoushismail@gmail.com>
2023-08-08 12:03:26 +0200

    adding some vavr utils

--- core/src/main/java/io/memoria/atom/core/utils/Vavrs.java
+++ core/src/main/java/io/memoria/atom/core/utils/Vavrs.java
@@ -4,9 +4,11 @@ import io.vavr.API;
 import io.vavr.Predicates;
 import io.vavr.collection.List;
 import io.vavr.collection.Traversable;
+import io.vavr.control.Option;
 import io.vavr.control.Try;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public final class Vavrs {
   private Vavrs() {}
@@ -35,5 +37,21 @@ public final class Vavrs {
       return tt.get().map(Try::success);
     else
       return List.of(Try.failure(tt.getCause()));
+  }
+
+  public static <T, E> Function<Option<T>, Try<Option<E>>> optToTryOpt(Function<T, Try<E>> fn) {
+    return opt -> optToTryOpt(opt, fn);
+  }
+
+  public static <T, E> Try<Option<E>> optToTryOpt(Option<T> option, Function<T, Try<E>> fn) {
+    if (option.isEmpty()) {
+      return Try.success(Option.none());
+    } else {
+      return fn.apply(option.get()).map(Option::some);
+    }
+  }
+
+  public static <T> Try<List<T>> toListOfTry(List<Try<T>> list) {
+    return Try.of(() -> list.map(Try::get));
   }
 }
