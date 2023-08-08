e46ba7eadbe6f4933f1d4171d388a481ddb59f5a
Ismail Marmoush <marmoushismail@gmail.com>
2023-08-08 11:36:41 +0200

    bug fix

--- testsuite/src/main/java/io/memoria/atom/testsuite/eventsourcing/banking/command/CloseAccount.java
+++ testsuite/src/main/java/io/memoria/atom/testsuite/eventsourcing/banking/command/CloseAccount.java
@@ -2,4 +2,4 @@ package io.memoria.atom.testsuite.eventsourcing.banking.command;
 
 import io.memoria.atom.eventsourcing.CommandMeta;
 
-public record CloseAccount(CommandMeta meta, long timestamp) implements AccountCommand {}
+public record CloseAccount(CommandMeta meta) implements AccountCommand {}
