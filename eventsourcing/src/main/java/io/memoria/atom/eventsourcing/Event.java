0bf39755d79922b8dbf22976bf39024db6292867
Ismail Marmoush <marmoushismail@gmail.com>
2023-12-22 10:35:28 +0100

    make commands, events, state shardable

--- eventsourcing/src/main/java/io/memoria/atom/eventsourcing/Event.java
+++ eventsourcing/src/main/java/io/memoria/atom/eventsourcing/Event.java
@@ -1,7 +1,19 @@
 package io.memoria.atom.eventsourcing;
 
+import io.memoria.atom.core.Shardable;
+import io.memoria.atom.core.Versioned;
+import io.memoria.atom.core.id.Id;
+
 import java.io.Serializable;
 
-public interface Event extends Serializable {
+public interface Event extends Shardable, Versioned, Serializable {
   EventMeta meta();
+
+  default @Override Id shardKey() {
+    return meta().shardKey();
+  }
+
+  default @Override long version() {
+    return meta().version();
+  }
 }
