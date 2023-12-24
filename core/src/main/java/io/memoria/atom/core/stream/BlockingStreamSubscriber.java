70a282bd22274f8d18b58560520deeb53026af4e
Ismail Marmoush <marmoushismail@gmail.com>
2023-12-24 10:27:13 +0100

    Removing Auto (#27)

--- core/src/main/java/io/memoria/atom/core/stream/BlockingStreamSubscriber.java
+++ core/src/main/java/io/memoria/atom/core/stream/BlockingStreamSubscriber.java
@@ -3,6 +3,6 @@ package io.memoria.atom.core.stream;
 import io.vavr.collection.Stream;
 import io.vavr.control.Try;
 
-public interface BlockingStreamSubscriber extends AutoCloseable {
+public interface BlockingStreamSubscriber {
   Try<Stream<Msg>> fetch(String topic, int partition);
 }
