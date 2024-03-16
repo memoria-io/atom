dcd47b95c87d2950c9f7058e0a2010e7b0f93b79
Ismail Marmoush <marmoushismail@gmail.com>
2024-03-16 17:37:10 +0100

    inherit from Exception

--- eventsourcing/src/main/java/io/memoria/atom/eventsourcing/ESException.java
+++ eventsourcing/src/main/java/io/memoria/atom/eventsourcing/ESException.java
@@ -1,6 +1,6 @@
 package io.memoria.atom.eventsourcing;
 
-public class ESException extends Throwable {
+public class ESException extends Exception {
   protected ESException(String msg) {
     super(msg);
   }
