5f2cce2db73cbea6a11d5bcef43dcd49ae321bd6
Ismail Marmoush <marmoushismail@gmail.com>
2024-03-17 20:20:06 +0100

    Fix code smell

--- core/src/main/java/io/memoria/atom/core/utils/Validations.java
+++ core/src/main/java/io/memoria/atom/core/utils/Validations.java
@@ -6,11 +6,12 @@ package io.memoria.atom.core.utils;
  * @see <a href="https://owasp.org/www-community/OWASP_Validation_Regex_Repository">OWASP Regex Repository </a>
  */
 public class Validations {
+  // nosonar
   public static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
 
   private Validations() {}
 
   public static boolean isValidEmail(String email) {
-    return email.matches(EMAIL_REGEX);
+    return email.length() < 320 && email.matches(EMAIL_REGEX);
   }
 }
