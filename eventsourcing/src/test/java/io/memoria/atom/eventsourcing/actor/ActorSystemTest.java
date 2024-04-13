b8658a87896cca3c66fad8492c796d5e652cc9a4
Ismail Marmoush <marmoushismail@gmail.com>
2024-04-13 19:07:03 +0200

    evolve test

--- eventsourcing/src/test/java/io/memoria/atom/eventsourcing/actor/ActorSystemTest.java
+++ eventsourcing/src/test/java/io/memoria/atom/eventsourcing/actor/ActorSystemTest.java
@@ -8,22 +8,34 @@ import io.memoria.atom.eventsourcing.command.exceptions.CommandException;
 import io.memoria.atom.eventsourcing.rule.CreateState;
 import io.memoria.atom.eventsourcing.rule.StateCreated;
 import io.memoria.atom.eventsourcing.state.StateId;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ActorSystemTest {
   @Test
-  void hashMapActorStore() {
+  void defaultSystem() {
     var actorStore = createStore();
     try (var actorSystem = ActorSystem.create(actorStore, new DomainActorFactory())) {
+      // Given
       StateId stateId = StateId.of(0);
       var meta = new CommandMeta(CommandId.of(UUID.randomUUID()), stateId);
+      // When
       var eventOpt = actorSystem.decide(stateId, new CreateState(meta));
-      Assertions.assertThat(eventOpt).isPresent().containsInstanceOf(StateCreated.class);
+      // Then
+      assertThat(eventOpt).isPresent().containsInstanceOf(StateCreated.class);
+      // And Given
+      var event = eventOpt.get();
+      // When
+      var state = actorSystem.evolve(stateId, event);
+      // Then
+      assertThat(state).isPresent().hasValueSatisfying(s -> {
+        assertThat(s.version()).isZero();
+      });
     } catch (IOException | CommandException e) {
       throw new RuntimeException(e);
     }
