20767bd4422abc9530a53b6ecbce3f1612273dd6
Ismail Marmoush <marmoushismail@gmail.com>
2025-03-27 05:16:59 +0100

    removing xcassandra

--- xcassandra/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.memoria.atom.xcassandra {
-  requires io.memoria.atom.core;
-  requires io.memoria.atom.eventsourcing;
-  requires java.driver.core;
-  requires java.driver.query.builder;
-}
