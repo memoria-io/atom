6a854f5f3f067481ed31cb18d12f66a47073346e
Ismail Marmoush <marmoushismail@gmail.com>
2025-04-26 17:49:18 +0200

    publishing sources and javadocs

--- xjackson/src/test/java/io/memoria/atom/jackson/JacksonJsonTest.java
+++ xjackson/src/test/java/io/memoria/atom/jackson/JacksonJsonTest.java
@@ -1,8 +1,10 @@
 package io.memoria.atom.jackson;
 
 import io.memoria.atom.core.text.TextException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,16 +12,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class JacksonJsonTest {
   @Test
   void serializeEngineer() {
-    var yamlEngineer = Tests.json.serialize(Resources.BOB_ENGINEER);
+    var yamlEngineerJson = Tests.json.serialize(Resources.BOB_ENGINEER);
     assert Resources.BOB_ENGINEER_JSON != null;
-    assertEquals(Resources.BOB_ENGINEER_JSON, yamlEngineer);
+    assertEquals(Resources.BOB_ENGINEER_JSON, yamlEngineerJson);
   }
 
   @Test
   void serializeManager() {
-    var yamlEngineer = Tests.json.serialize(Resources.ANNIKA_MANAGER);
+    var engineerYaml = Tests.json.serialize(Resources.ANNIKA_MANAGER);
     assert Resources.ANNIKA_MANAGER_JSON != null;
-    assertEquals(Resources.ANNIKA_MANAGER_JSON, yamlEngineer);
+    assertEquals(Resources.ANNIKA_MANAGER_JSON, engineerYaml);
+  }
+
+  @Test
+  void serializeEmployees() {
+    var employeesJson = Tests.json.serialize(Resources.EMPLOYEES);
+    assert Resources.EMPLOYEES_JSON != null;
+    assertEquals(Resources.EMPLOYEES_JSON, employeesJson);
   }
 
   @Test
@@ -50,5 +59,13 @@ class JacksonJsonTest {
     // Then
     assertEquals(Resources.ANNIKA_MANAGER, manager);
     assertEquals(List.of(Resources.BOB_ENGINEER, Resources.ALEX_ENGINEER), manager.team());
+  }
+
+  @Test
+  void deserializeEmployees() throws TextException {
+    // When
+    var employees = Tests.json.deserialize(Resources.EMPLOYEES_JSON, Employee[].class);
+    // Then
+    Assertions.assertThat(Resources.EMPLOYEES).hasSameElementsAs(Arrays.stream(employees).toList());
   }
 }
--- xjackson/src/test/java/io/memoria/atom/jackson/Resources.java
+++ xjackson/src/test/java/io/memoria/atom/jackson/Resources.java
@@ -14,12 +14,15 @@ public class Resources {
   public static final String ANNIKA_MANAGER_JSON;
   public static final String DEPARTMENT_JSON;
   public static final String NAME_CREATED_JSON;
+  public static final String EMPLOYEES_JSON;
+
   // Yaml Resources
   public static final String BOB_ENGINEER_YAML;
   public static final String ANNIKA_MANAGER_YAML;
   public static final Engineer BOB_ENGINEER;
   public static final Engineer ALEX_ENGINEER;
   public static final Manager ANNIKA_MANAGER;
+  public static final Employee[] EMPLOYEES;
 
   static {
     try {
@@ -29,13 +32,14 @@ public class Resources {
       ANNIKA_MANAGER_JSON = FileOps.readResource("cases/company/json/Manager.json");
       DEPARTMENT_JSON = FileOps.readResource("cases/company/json/Department.json");
       NAME_CREATED_JSON = FileOps.readResource("cases/company/json/NameCreated.json");
+      EMPLOYEES_JSON = FileOps.readResource("cases/company/json/Employees.json");
       BOB_ENGINEER_YAML = FileOps.readResource("cases/company/yaml/Engineer.yaml");
       ANNIKA_MANAGER_YAML = FileOps.readResource("cases/company/yaml/Manager.yaml");
-
       // Objects
       BOB_ENGINEER = new Engineer(Ids.of(0), "bob", LocalDate.of(2000, 1, 1), List.of("fix issue 1", "Fix issue 2"));
       ALEX_ENGINEER = new Engineer(Ids.of(1), "alex", LocalDate.of(2000, 1, 1), List.of("fix issue 3", "Fix issue 4"));
       ANNIKA_MANAGER = new Manager("Annika", List.of(BOB_ENGINEER, ALEX_ENGINEER));
+      EMPLOYEES = new Employee[]{BOB_ENGINEER, ANNIKA_MANAGER};
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
